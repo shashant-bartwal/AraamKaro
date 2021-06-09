8f40abf391cf1a92abf384d7a5848085922b50e3
NARENDRA ARORA <narendra.arora@evontech.com>
2021-06-09 03:51:35 -0700

    Initial Commit

---
+++ AraamKaro/AraamKaroApp.swift
@@ -0,0 +1,17 @@
+//
+//  AraamKaroApp.swift
+//  AraamKaro
+//
+//  Created by shashant on 08/06/21.
+//
+
+import SwiftUI
+
+@main
+struct AraamKaroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
